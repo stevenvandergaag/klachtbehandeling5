7a5b2253a69b3e099b8fc89bac2fb340439da765
steven56-spec <steven.vandergaag@ictmbo.nl>
2021-09-27 13:51:28 +0200

    update

--- src/index.php
+++ src/index.php
@@ -1,65 +1,35 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-?>
 <!doctype html>
-<html lang="nl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        form {
-            text-align: center;
-            align-content: center;
-        }
-
-
-    </style>
-    <title>klachten form</title>
+    <title>Document</title>
 </head>
 <body>
-<form action="" method="post">
-    <label for="naam">Naam:</label>
-    <input type="text" id="naam" name="naam" placeholder="vul je naam in"><br>
-    <label for="email">Email</label>
-    <input type="email" id="email" name="email" placeholder="vul je email in"><br>
-    <label for="onderwerp">Onderwerp</label>
-    <input type="text" id="onderwerp" name="onderwerp" placeholder="onderwerp"><br>
-    <p>Je klacht</p>
-    <textarea name="klacht" id="klacht" rows="4"></textarea><br>
-    <input type="submit" value="submit">
+<div style="text-align: center;">
+    <form action="mail.php" method="POST">
+        <h2>Send an email</h2>
 
-
+        <label>name</label>
+        <label for="name"></label><input id="name" name="name" type="text" placeholder="Enter name">
+        <br><br>
+        <label>email</label>
+        <label for="email"></label><input id="email"  name="email" type="text" placeholder="Enter email">
+        <br><br>
+        <label>klacht</label>
+        <label for="subject"></label><input id="subject" name="subject" type="text" placeholder="Enter subject">
+        <br><br>
+        <p>message</p>
+        <label for="body"></label><textarea id="body" name="body" rows="5" placeholder="type message"></textarea>
+        <br><br>
+        <input type="submit" value="verstuur">
 </form>
+</div>
+
 </body>
 </html>
-<?php
-require '../vendor/autoload.php';
-if (isset($_POST['email'])) {
 
-    $email = $_POST['email'];
-    $naam = $_POST['naam'];
-    $onderwerp = $_POST['onderwerp'];
-    $klacht = $_POST['klacht'];
 
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->Host = 'smtp.mailtrap.io';
-    $mail->Subject = $onderwerp;
-    $mail->SMTPAuth = true;
-    $mail->Port = 2525;
-    $mail->Username = 'aae80984fe9b5f';
-    $mail->Password = '00e8685ea1b58e';
-    $mail->isHTML(true);
-    $mail->From = "Lucasdh2003@gmail.com";
-    $mail->FromName = "LDH-support";
-    $mail->msgHTML($klacht);
-    $mail->addAddress($email);
-    $mail->AddCC("40188047@roctilburg.nl");
-    if (!$mail->send()) {
-        echo "mailer Error: " . $mail->ErrorInfo;
-    } else {
-        echo "Mail is gestuurd!";
-    }
-} ?>
+
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,14 +9,27 @@ class ComposerStaticInit8fda963abd32d7a1e6b5d652f2bb2298
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
+            'Psr\\Log\\' => 8,
             'PHPMailer\\PHPMailer\\' => 20,
+        ),
+        'M' => 
+        array (
+            'Monolog\\' => 8,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+        ),
         'PHPMailer\\PHPMailer\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+        'Monolog\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
         ),
     );
 
